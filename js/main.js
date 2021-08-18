0a6ee5ee2acc84dc8205d59f71851501394a0248
yuras <work.sergeevv@gmail.com>
2021-08-18 14:01:41 +0300

    perivat logic

---
+++ js/main.js
@@ -0,0 +1,88 @@
+window.onload = function () {
+    let tab_1 = document.querySelector('.tab_1');
+    let tab_2 = document.querySelector('.tab_2');
+    let content_1 = document.querySelector('.content_1');
+    let content_2 = document.querySelector('.content_2');
+    let buttonOk = document.querySelector('.btn_ok');
+    let countIn = document.querySelector('.count-in_js');
+    let numberIn = document.querySelector('.number-in_js');
+    let dateOut    = document.querySelector('.date-out_js');
+    let timeOut    = document.querySelector('.time-out_js');
+    let timerOut    = document.querySelector('.timer-out_js');
+    let numberOut    = document.querySelector('.number-out_js');
+    let countOut    = document.querySelector('.count-out_js');
+
+    const date = new Date();
+    const dayNow =  date.getDate();
+    const monthNow = date.getMonth() + 1;
+    const yearNow = date.getFullYear();
+    const hourNow = date.getHours();
+    const minuteNow = date.getMinutes();
+    const secondNow = date.getSeconds();
+
+    let timerMinute = 57;
+    let timerSecond = 60;
+
+
+    tab_1.addEventListener('click',firstTab);
+    tab_2.addEventListener('click',secondTab)
+
+    buttonOk.addEventListener('click',function (){
+       if(numberIn.value && countIn.value){
+           countOut.innerText = countIn.value + ' шт';
+           numberOut.innerText = '№ '+numberIn.value;
+           countIn.value='';
+           numberIn.value='';
+           timeOut.innerText = setTime(trueDate(hourNow),trueDate(minuteNow),trueDate(secondNow));
+           dateOut.innerText = setDay(trueDate(dayNow),trueDate(monthNow),trueDate(yearNow));
+           timerInterval();
+           firstTab();
+       }
+    })
+
+
+    function firstTab (){
+        tab_2.classList.remove('active');
+        tab_1.classList.add('active');
+        content_2.classList.remove('active');
+        content_1.classList.add('active');
+    };
+
+    function secondTab (){
+        tab_1.classList.remove('active');
+        tab_2.classList.add('active');
+        content_1.classList.remove('active');
+        content_2.classList.add('active');
+    };
+
+    function setTime(hour,minute, second){
+         return ( hour+':'+minute+':'+second);
+    }
+    function setDay(day,month, year){
+        return ( day+'.'+month+'.'+year);
+    }
+    function timerInterval(){
+        setInterval(function (){
+
+            timerSecond--;
+            timerOut.innerText = (trueDate(timerMinute)+':'+trueDate(timerSecond));
+            if(timerSecond == 0){
+                timerMinute--;
+                timerSecond= 60;
+            }
+             if(timerMinute == 0){
+                 timerMinute = 20;
+            }
+        },1000)
+    }
+};
+
+function trueDate(number){
+    if(number<10){
+        return('0'+number)
+    }
+    else{
+        return (''+number)
+    }
+}
+
